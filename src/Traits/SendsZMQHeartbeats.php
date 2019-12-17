7b2e7ce151faa848620e4f0d3ef303057afdec0d
Nikolaj Boel Jensen <nbj@cego.dk>
2019-12-17 13:50:21 +0100

    Make sure max int value is correct

--- src/Traits/SendsZMQHeartbeats.php
+++ src/Traits/SendsZMQHeartbeats.php
@@ -62,7 +62,7 @@ trait SendsZMQHeartbeats
             $this->resetCounter = false;
         }
 
-        // We limit the max integer size to the equivalent of a signed 32bit integer (2.147.483.647)
+        // We limit the max integer size to the equivalent of a signed 32bit integer (2.147.483.648)
         $maxInt = pow(2, 32) / 2;
 
         // Increment the heartbeat count and make sure we rollover when we reach our max integer size
